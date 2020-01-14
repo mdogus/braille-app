ebeb9f2c30121d54a8778d7b19b0c3cb5dcf07fa
Mustafa Doğuş <mustafadogus45@gmail.com>
2020-01-14 21:08:02 +0300

    Updated Practice ViewController.

---
+++ BrailleQuiz/View Controllers/LearnTabViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LearnTabViewController.swift
+//  BrailleQuiz
+//
+//  Created by MUSTAFA DOĞUŞ on 14.01.2020.
+//  Copyright © 2020 MUSTAFA DOĞUŞ. All rights reserved.
+//
+
+import UIKit
+
+class LearnTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
